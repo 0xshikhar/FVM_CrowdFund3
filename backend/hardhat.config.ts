4275905c290a46a219fe6fee93317049ffa06f35
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-05-07 00:48:06 +0530

    updated design elements

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -18,39 +18,51 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   networks: {
     hardhat: {
       chainId: 1337,
       allowUnlimitedContractSize: true,
     },
+    bnb_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      allowUnlimitedContractSize: true,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    bnb_mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/Kd1XQbFAa3ZboKORKFNQ9mmtcrM5PbZv",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    },
-    mantle_testnet: {
-      url: "https://rpc.testnet.mantle.xyz/",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     mumbai: {
       url: "https://polygon-mumbai.g.alchemy.com/v2/vG6GH-5Gha7RcZ6hKud-oyraJ8aTyUxB",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-  },
-  // gasReporter: {
-  //   enabled: process.env.REPORT_GAS !== undefined,
-  //   currency: "USD",
-  // },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+
+    // gasReporter: {
+    //   enabled: process.env.REPORT_GAS !== undefined,
+    //   currency: "USD",
+    // },
+
   },
 };
 
--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,6 +1,6 @@
 import createCampaign from './create-campaign.svg';
 import dashboard from './dashboard.svg';
-import logo from './logo.svg';
+import logo from './crowdfund-1-bg.png';
 import logout from './logout.svg';
 import payment from './payment.svg';
 import profile from './profile.svg';
